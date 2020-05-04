8419417b26a61bd9a674adb759c1834c790f3cf3
garcia718gt4 <58898368+garcia718gt4@users.noreply.github.com>
2020-05-04 07:58:36 -0700

    final version - meets expectations

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 // Global variables 
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-const btnReset = document.querySelector('a.btn__reset');
+const btnReset = document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
-const ul = document.querySelector('ul');
+let hearts = document.querySelector('li img');
 
-const missed = 0; 
 
-const phrases = [
+let missed = 0; 
+
+let phrases = [
     'beat around the bush',
     'working hard or hardly working', 
     'better late than never',
@@ -20,70 +21,104 @@ const phrases = [
         Project Functions 
  ================================ */
 
-/** 
-* Returns a random phrase from an array.
-* @param {array} arr - an array of phrases.
-* @returns {string} a random string from the array. 
-*/
-const getRandomPhrasesAsArray = arr => {
-    let randomNum = Math.floor(Math.random() * arr.length);
-    return arr[randomNum];
+// listen for the start game button to be pressed
+btnReset.addEventListener('click', () => {
+    overlay.style.display = 'none'  
+
+    const randomPhrase = getRandomPhrasesAsArray(phrases);
+    addPhraseToDisplay(randomPhrase);
+}); 
+
+
+function getRandomPhrasesAsArray (phrase) {
+    let randomNum = Math.floor(Math.random() * phrase.length);
+    let newPhrase = phrase[randomNum]; 
+    let randomPhrase = newPhrase.split('');
+    return randomPhrase; 
 }
-// call the function and the return value in a variable; 
-const randomPhrase = getRandomPhrasesAsArray(phrases);
+
 
 // adds the individual letters of a string  
 // as list items to the display
-const addPhraseToDisplay = phrase => {
-    for(let i = 0; i < phrase.length; i++){
-        let li = document.createElement('li');
-        li.textContent = phrase[i]; 
-        ul.appendChild(li);
+function addPhraseToDisplay (arr){
+    const ul = document.querySelector('#phrase ul');
+
+    for (let i = 0; i < arr.length; i++) {  
+        const li = document.createElement('li');
+        li.textContent = arr[i]; 
         if(li.textContent !== " ") {
-            li.className = 'letter';
+            li.className += ' letter';
         } else {
-            li.className = 'space';
+            li.className += ' space';
         } 
+        ul.appendChild(li); 
     }
 }
-// call the function 
-addPhraseToDisplay(randomPhrase);
+
 
 
 // check if a letter is in the phrase 
-const checkLetter = button => {
-    let li = ul.children; 
-    let match = null;
-    for (let i = 0; i < li.length; i++) {
-        if (li[i].className === 'letter') {
-            if (li[i].textContent === button){ 
-                li[i].className = 'show'; 
-                match = li[i]; 
-            }
-        } 
+function checkLetter(button){  
+    const lis = document.querySelectorAll('.letter');
+    let match = null;  
+    for (let i = 0; i < lis.length; i++) {
+        const character = lis[i].textContent; 
+         if (button.textContent === character){ 
+                lis[i].className += ' show'; 
+                match = character;
+            }   
     } 
-    return match; 
+        if (match !== '') {
+            return match; 
+        } else {
+            return null; 
+        }
+    
 }
 
-
-// check if the game has been won or lost
-const checkWin = () => {
-
-}
-
-// listen for the start game button to be pressed
-btnReset.addEventListener('click', () => {
-    overlay.style.display = 'none';  
-}); 
 
 // listen for the onscreen keyboard to be clicked
 qwerty.addEventListener('click', e => {
-    let button = e.target;
-    if (button.tagName === 'BUTTON') {
-        char = button.textContent; 
-        button.className = 'chosen';
-        button.setAttribute('disabled', 'true');
-     
-    }
-    
+    let button_press = e.target; 
+    if (button_press.tagName === 'BUTTON') {
+        button_press.className += ' chosen';
+        button_press.setAttribute('disabled', 'true'); 
+        const letterFound = checkLetter(button_press);
+        if (letterFound === null) {
+            // remove a heart
+            const hearts = document.querySelectorAll('#scoreboard img');
+            const lostHeart = hearts[missed];
+            lostHeart.setAttribute('src', 'images/lostHeart.png');
+            missed += 1; 
+         }
+         checkWin();
+    }  
+   
 }); 
+
+// check if the game has been won or lost
+function checkWin () {
+    const li_show = document.querySelectorAll('#phrase .show');
+    const li_letter = document.querySelectorAll('#phrase .letter'); 
+    const message = document.querySelector('#overlay .title');
+
+    function results () {
+        phrase.style.display = 'none'
+        overlay.style.display = 'flex'; 
+        btnReset.style.display = 'none'; 
+    }
+        if (li_letter.length === li_show.length) {
+            overlay.className = 'win';
+            message.textContent = 'You won!'; 
+            results();
+        } else if (missed > 4) {
+            overlay.className = 'lose'; 
+            message.textContent = 'You lost :(';
+            results(); 
+         }
+
+}
+
+
+
+
